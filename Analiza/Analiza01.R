72361e9c253c4d8a1502f23ab399efd4b96f98da
Linras <linras@wp.pl>
2019-12-01 18:19:36 +0100

    Working on dataset, basic statistics and diagnostic diagrams.

--- Analiza/Analiza01.R
+++ Analiza/Analiza01.R
@@ -1,9 +1,11 @@
 library(tidyverse)
+library(fitdistrplus)
 # set working directory
 path_loc <- "C:/repo/ModZdEkstr/Analiza"
 setwd(path_loc)
 # reading in the data
 df <- read_csv("weight-height.csv") ; df
+df <- df[seq(1, nrow(df), 50), ]
 
 # taking a quick look
 glimpse(df)
@@ -20,3 +22,35 @@ df <- df %>% as_tibble() %>% mutate(
   Weight = Weight / 2.205
 )
 
+data <- df$Weight
+
+# średnia:
+mean(data)
+# odchylenie standardowe / standard duration
+sd(data)
+# skośność / skewness oraz kurtoza
+library(moments)
+skewness(data)
+kurtosis(data)
+
+# histogram 
+#plotdist(data, histo = TRUE, demp = TRUE)
+
+# wykres skośność- kurtoza
+descdist(data, boot = 200)
+
+# przygotowanie pod wykresy diagnostyczne
+fw <- fitdist(data, "weibull")
+fg <- fitdist(data, "gamma")
+fln <- fitdist(data, "lnorm")
+summary(fw)
+summary(fg)
+summary(fln)
+
+# wykresy diagnostyczne
+par(mfrow = c(1,1))
+plot.legend <- c("Weibull", "lognormal", "gamma")
+denscomp(list(fw, fln, fg), legendtext = plot.legend)
+qqcomp(list(fw, fln, fg), legendtext = plot.legend)
+cdfcomp(list(fw, fln, fg), legendtext = plot.legend)
+ppcomp(list(fw, fln, fg), legendtext = plot.legend)
